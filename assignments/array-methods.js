00ccf4da885b4ac926bf94f1d0d7db340914fb5e
Mary <mary.clayton1@hotmail.com>
2019-08-20 20:31:15 -0700

    completed assignments

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,29 +57,47 @@ const runners = [
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
-let fullNames = [];
+const fullNames = runners.forEach(function(names){
+  return `${names.first_name} ${names.last_name}`;
+});
 console.log(fullNames);
 
-// ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
+const firstNamesAllCaps = runners.map (function (item){
+  return item.first_name.toUpperCase();
+});
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
+let runnersLargeSizeShirt= runners.filter(function(items){
+  return items.shirt_size === "L";
+});
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = runners.reduce(function(accumulator, item){
+  return accumulator + item.donation;
+},0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+const catShelter = [{'name':"Ducky", 'color': "black" , 'speak': "I like to meow"}, {'name':"minnieme", 'color': 'black', 'speak': "I like to eat."}];
+const catHouse = catShelter.forEach(function(cats){
+return cats.name.toUpperCase(); 
+});
+console.log(catHouse);
+  // console.log(currentValue.state);
 // Problem 2
-
+const catColor = catShelter.filter(function(cats){
+  return cats.color; 
+  });
+  console.log(catColor);
 // Problem 3
+const catTalk= catShelter.map(function(cats){
+  return cats.speak; 
+  });
+  console.log(catTalk);
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,68 +2,96 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  // GIVEN THIS PROBLEM:
 
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // // GIVEN THIS PROBLEM:
 
-  // SOLUTION:
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // // SOLUTION:
 
-  // NOTES ON THE SOLUTION:
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
-  // firstItem is a higher order function.
-  // It expects a callback (referred to as `cb`) as its second argument.
-  // To test our solution, we can use the given `items` array and a variety of callbacks.
-  // Note how callbacks can be declared separately, or inlined.
+  // // NOTES ON THE SOLUTION:
 
-  // TEST 1 (inlined callback):
+  // // firstItem is a higher order function.
+  // // It expects a callback (referred to as `cb`) as its second argument.
+  // // To test our solution, we can use the given `items` array and a variety of callbacks.
+  // // Note how callbacks can be declared separately, or inlined.
 
-  const test1 = firstItem(items, item => `I love my ${item}!`);
-  console.log(test1); // "I love my Pencil!"
+  // // TEST 1 (inlined callback):
 
-  // TEST 2 (declaring callback before hand):
+  // const test1 = firstItem(items, item => `I love my ${item}!`);
+  // console.log(test1); // "I love my Pencil!"
 
-  function logExorbitantPrice(article) {
-    return `this ${article} is worth a million dollars!`;
-  };
+  // // TEST 2 (declaring callback before hand):
 
-  const test2 = firstItem(items, logExorbitantPrice);
-  console.log(test2); // "this Pencil is worth a million dollars!"
-*/
+  // function logExorbitantPrice(article) {
+  //   return `this ${article} is worth a million dollars!`;
+  // };
+
+  // const test2 = firstItem(items, logExorbitantPrice);
+  // console.log(test2); // "this Pencil is worth a million dollars!"
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
-
+console.log(getLength(items, function(length){
+  return length;
+}))
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.pop() );
 }
+console.log(last(items, function(lastItem){
+  return lastItem;
+}));
+
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+const sum =function(x, y) {
+return x + y;
+};
+console.log(sumNums(3,6, sum));
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+const multiply =function(x, y) {
+return cb(x+y);
+};
+console.log(multiplyNums(20,6, function(mult){
+  return mult;
+}));
+  // multiplyNums multiplies two numbers and passes the result to the callback.
 
-function contains(item, list, cb) {
+
+
+  function contains(item, list, cb) {
+  return list.includes(item);
+  };
+  console.log(contains("notebook", []));
+  
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+
+  /* STRETCH PROBLEM */
+function removeDuplicates(){
+
 }
 
-/* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-}
